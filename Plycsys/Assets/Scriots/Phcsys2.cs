aceb81d30000f2be2cc5935fad8025cb4b674da1
hayabusa09060 <hayabusableach@gmail.com>
2020-05-28 10:48:15 +0900

    aaa

--- Plycsys/Assets/Scriots/Phcsys2.cs
+++ Plycsys/Assets/Scriots/Phcsys2.cs
@@ -6,13 +6,15 @@ public class Phcsys2 : MonoBehaviour
 {
     public float m;
     public float v,x;
+    private float savev, savem;
     public GameObject gameObject;
     private float distance;
     public bool lr;
+    public bool check;
     // Start is called before the first frame update
     void Start()
     {
-        
+        savev = v;
     }
 
     // Update is called once per frame
@@ -21,27 +23,32 @@ public class Phcsys2 : MonoBehaviour
         if (x > 10&&lr)
         {
             v *= -1;
-            lr = false;
         }
         if (x < -10&&!lr)
         {
             v *= -1;
-            lr = true;
-        }
-        x =x+m*v;
-
-
-        distance = Vector3.Distance(transform.position, gameObject.transform.position);
-
-        if(distance<=1)
-        {
-
-                v = ((m - gameObject.GetComponent<Phcsys2>().m) * v + 2 * gameObject.GetComponent<Phcsys2>().m  * gameObject.GetComponent<Phcsys2>().v) / (m + gameObject.GetComponent<Phcsys2>().m);
-                gameObject.GetComponent<Phcsys2>().v = ((gameObject.GetComponent<Phcsys2>().m-m) * gameObject.GetComponent<Phcsys2>().v + 2 * m * v) / (m + gameObject.GetComponent<Phcsys2>().m);
             
-            Debug.Log(distance);
-           // v = ((m - gameObject.GetComponent<Phcsys2>().m) * v + 2 * gameObject.GetComponent<Phcsys2>().m * 2 * gameObject.GetComponent<Phcsys2>().v) / (m +  gameObject.GetComponent<Phcsys2>().m);
         }
+
+
+
+        distance = Vector3.Distance(gameObject.transform.position, transform.position);
+
+        if(check)
+        {
+            if (distance <= 1.1&&distance>0.9f)
+            {
+
+                savev = ((m - gameObject.GetComponent<Phcsys2>().m) * v + 2 * gameObject.GetComponent<Phcsys2>().m * gameObject.GetComponent<Phcsys2>().v) / (m + gameObject.GetComponent<Phcsys2>().m);
+                gameObject.GetComponent<Phcsys2>().v = ((gameObject.GetComponent<Phcsys2>().m - m) * gameObject.GetComponent<Phcsys2>().v + 2 * m * v) / (m + gameObject.GetComponent<Phcsys2>().m);
+                v = savev;
+                //v *= -1;
+                //gameObject.GetComponent<Phcsys2>().v *= -1;
+                Debug.Log(distance);
+                // v = ((m - gameObject.GetComponent<Phcsys2>().m) * v + 2 * gameObject.GetComponent<Phcsys2>().m * 2 * gameObject.GetComponent<Phcsys2>().v) / (m +  gameObject.GetComponent<Phcsys2>().m);
+            }
+        }
+        x = x + m * v;
         transform.position = new Vector3(x, 0, 0);
     }
 }
