53ef8a0e3828ece2de5b3be9e426d85b5e155115
hayabusa09060 <hayabusableach@gmail.com>
2020-06-11 09:46:27 +0900

    aaa

---
+++ Plycsys/Assets/Scriots/Phcsys3.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phcsys3 : MonoBehaviour
+{
+    public float m;
+    public float v,x,y;
+    private Vector3 savev;
+    private float savem;
+    public GameObject gameObject;
+    private float distance;
+    public bool lr;
+    public bool check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //savev = v;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x > 10&&lr)
+        {
+            v *= -1;
+        }
+        if (x < -10&&!lr)
+        {
+            v *= -1;
+            
+        }
+        if (y < -5 && !lr)
+        {
+            v *= -1;
+
+        }
+        if (x > 5 && !lr)
+        {
+            v *= -1;
+
+        }
+
+
+        distance = Vector3.Distance(gameObject.transform.position, transform.position);
+
+        if(check)
+        {
+            if (distance <= 1.1&&distance>0.9f)
+            {
+
+                savev = (transform.position-gameObject.transform.position);
+                gameObject.GetComponent<Phcsys3>().v = ((gameObject.GetComponent<Phcsys3>().m - m) * gameObject.GetComponent<Phcsys3>().v + 2 * m * v) / (m + gameObject.GetComponent<Phcsys3>().m);
+                //v *= -1;
+                //gameObject.GetComponent<Phcsys2>().v *= -1;
+                Debug.Log(distance);
+                // v = ((m - gameObject.GetComponent<Phcsys2>().m) * v + 2 * gameObject.GetComponent<Phcsys2>().m * 2 * gameObject.GetComponent<Phcsys2>().v) / (m +  gameObject.GetComponent<Phcsys2>().m);
+            }
+        }
+        x = x + m * v;
+        y = y + m * v;
+        transform.position = new Vector3(x, y, 0);
+    }
+}
---
+++ Plycsys/Assets/Scriots/Phycsys4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phycsys4 : MonoBehaviour
+{
+    public float ax, ay, kv, m, g, vx, vy, px, py, kx, fx, fy, rx, ry, G;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kv = kv + ax;
+        py = py + kv;
+
+
+        var angles = transform.rotation.eulerAngles;
+        angles.z += ax;
+        transform.rotation = Quaternion.Euler(angles);
+    }
+}
