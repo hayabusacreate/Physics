e3e140593edb4504101d280ea55ff160bc7df64c
大石　隼 <>
2020-08-27 09:37:50 +0900

    osakana

---
+++ Plycsys/Assets/hebi/body.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class body : MonoBehaviour
+{
+    private GameObject oya;
+    private float x, y;
+    public float far;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oya = gameObject.transform.root.gameObject;
+        gameObject.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = gameObject.transform.position.x - oya.transform.position.x;
+        y = gameObject.transform.position.y - oya.transform.position.y;
+        if ((x < -far || x > far) || (y < -far || y > far))
+        {
+            transform.position -= new Vector3(x / 10, y / 10);
+        }
+    }
+}
---
+++ Plycsys/Assets/hebi/hebi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hebi : MonoBehaviour
+{
+    public int body;
+    public GameObject bady;
+    private Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject oya = gameObject;
+        for(int i=0;i<body;i++)
+        {
+            GameObject body= Instantiate(bady, gameObject.transform.position, Quaternion.identity);
+            body.transform.parent = oya.transform;
+            oya = body;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = Input.mousePosition;
+        position.z -= -10;
+        gameObject.transform.position= Camera.main.ScreenToWorldPoint(position);
+
+    }
+}
